ad64a5e78df5a3258834e22daef9d767f5a2304b
Harshal Thakare <harshthakare2499@gmail.com>
2023-05-17 14:52:29 +0530

    eg

---
+++ Day4/add.js
@@ -0,0 +1,8 @@
+function sum(){
+    let input1 = document.querySelector("#id1").value;
+    let inout2 = document.querySelector("#id2").value;
+
+    let result = parseInt(input1) + parseInt(inout2);
+
+    alert("Input 1 ="+input1+" Inout 2 = "+inout2+" Result = "+result);
+}
---
+++ Day5/paragraph.js
@@ -0,0 +1,8 @@
+function colours(p1){
+    let ptag=document.querySelector("p");
+    ptag.style.color=p1;
+    
+    }
+    function applyFont(p1) {
+        let ptag = document.querySelector("p");
+        ptag.style.fontSize = p1;}
